fc3270f86902584c7ccf23731d5ba081b82ed37b
AnshShinde2007 <anshshinde449@gmail.com>
2026-01-05 16:43:15 +0530

    fix: remove static export and add Google images support

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   
   async headers() {
     return [
@@ -16,6 +15,16 @@ const nextConfig: NextConfig = {
         ],
       },
     ];
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
